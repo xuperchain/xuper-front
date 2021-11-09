8438c757bf4c5a5551cfc115c46ace5cf1fa61b9
springrain <chunanyong@163.com>
2021-11-09 18:43:08 +0800

    支持国密

--- crypto/client.go
+++ crypto/client.go
@@ -4,10 +4,40 @@
 package crypto
 
 import (
+	"encoding/json"
+	"io/ioutil"
+	"strings"
+
+	"github.com/xuperchain/crypto/client/service/base"
+	"github.com/xuperchain/crypto/client/service/gm"
 	"github.com/xuperchain/crypto/client/service/xchain"
+	"github.com/xuperchain/crypto/gm/account"
+	"github.com/xuperchain/xuper-front/config"
 )
 
-// GetCryptoClient get crypto client
-func GetCryptoClient() *xchain.XchainCryptoClient {
-	return &xchain.XchainCryptoClient{}
+func GetCryptoClient() base.CryptoClient {
+	xcc := new(xchain.XchainCryptoClient)
+	return base.CryptoClient(xcc)
+}
+
+func GetGMCryptoClient() base.CryptoClient {
+	xcc := new(gm.GmCryptoClient)
+	return base.CryptoClient(xcc)
+}
+
+func KeyIsGM() (bool, error) {
+	keyPEMBlock, err := ioutil.ReadFile(config.GetKeys() + "public.key")
+	if err != nil {
+		return false, err
+	}
+	ecdsaPublicKey := &account.ECDSAPublicKey{}
+	err = json.Unmarshal(keyPEMBlock, ecdsaPublicKey)
+	if err != nil {
+		return false, err
+	}
+	if strings.Contains(ecdsaPublicKey.Curvname, "SM") {
+		return true, nil
+	} else {
+		return false, nil
+	}
 }
